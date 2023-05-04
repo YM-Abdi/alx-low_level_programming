dafaa484fd3c50a0bf8c45042e83e4a528f93d83
YM-Abdi <yusufmohammed.abdi1@gmail.com>
2023-05-04 11:18:31 -0700

    All

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int max = 0x01;
+
+	max = ~(max << index);
+	if (max == 0x00)
+		return (-1);
+	*n &= max;
+	return (1);
+}
